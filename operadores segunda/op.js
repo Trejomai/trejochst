5b17f790e818fcd9935425c3c2050b875a0c5a2c
Trejomai <108032270+Trejomai@users.noreply.github.com>
2022-10-01 22:39:03 -0300

    .
    .

---
+++ operadores segunda/op.js
@@ -0,0 +1,9 @@
+const herramienta = prompt ('Ingrese una herramienta');
+const color = prompt ('Ingrese un color');
+
+if (herramienta === 'martillo' && color === 'rojo' ) {
+    alert('Adivinamos la combinacion');
+
+} else {
+    alert('No adivinamos la combinacion');
+}
